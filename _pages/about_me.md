3f1722c2170a866f6754f55ccf2757f10c3d5870
eiroW <water_wowng@icloud.com>
2025-09-12 10:59:48 +0000

    Deploying to gh-pages from @ eiroW/eiroW.github.io@af864310cb8c4527a4e4cef81339d6285ada714c 🚀

--- _pages/about_me.md
+++ _pages/about_me.md
@@ -3,4 +3,3 @@ Junyi Wang is a master's student (Class of 2023) at the School of Information an
 His research has led to publications in leading journals such as *MedAI* and *NeuroImage*, as well as four papers at international conferences including **MICCAI**, **ISBI**, and **OHBM**, with one paper selected for **MICCAI 2025 Spotlight**.  
 
 Junyi has also demonstrated strong leadership in academic competitions, earning top honors such as the **“Challenge Cup” Grand Prize** and first place in the **MICCAI Learn2Reg Challenge**. His work bridges cutting-edge algorithms with clinical applications, aiming to advance precision medicine through intelligent medical imaging technologies.
-
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -421,16 +407,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+      },{id: "news-a-paper-is-accepted-as-the-miccai-spotlight",
+          title: 'A paper is accepted as the MICCAI spotlight',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
